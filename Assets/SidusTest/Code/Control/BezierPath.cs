6f1fa0b22e74dcd7038e70c98a0da4ffa2174ab2
Kirill Smirnov <thegaldren@gmail.com>
2022-03-16 19:41:59 +0400

    Make BezierPath.cs continue curves

--- Assets/SidusTest/Code/Control/BezierPath.cs
+++ Assets/SidusTest/Code/Control/BezierPath.cs
@@ -1,6 +1,4 @@
-using System;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace SidusTest.Control
 {
@@ -22,14 +20,24 @@ namespace SidusTest.Control
 
         private void PreparePathImpl(Vector3 from, Vector3 to)
         {
+            var continueCurve = Vector3.Distance(from, _bezierPoints[3]) < float.Epsilon;
             _bezierPoints[0] = from;
             _bezierPoints[3] = to;
             var middlePoint = (from + to) / 2;
             var middle1 = (from + middlePoint) / 2;
             var middle2 = (to + middlePoint) / 2;
-            _bezierPoints[1] = Random.Range(bezierRadius.x, bezierRadius.y) * Random.onUnitSphere + middle1;
-            _bezierPoints[2] = Random.Range(bezierRadius.x, bezierRadius.y) * Random.onUnitSphere + middle2;
+            _bezierPoints[1] = continueCurve ? MirrorPoint(_bezierPoints[2], from) : ShiftPoint(middle1);
+            _bezierPoints[2] = ShiftPoint(middle2);
         }
+
+        private Vector3 MirrorPoint(Vector3 previous, Vector3 current)
+        {
+            var next = 2 * current - previous;
+            return next;
+        }
+
+        private Vector3 ShiftPoint(Vector3 middle1) 
+            => Random.Range(bezierRadius.x, bezierRadius.y) * Random.onUnitSphere + middle1;
 
         public static Vector3 PositionAt(float t)
             => _instance.PositionAtImpl(t);
