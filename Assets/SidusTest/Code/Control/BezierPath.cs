8c1ca752c5b338436a8368517e7d116d1a293c0a
Kirill Smirnov <thegaldren@gmail.com>
2022-03-16 20:19:05 +0400

    Downgrade BezierPath.cs to quadratic

--- Assets/SidusTest/Code/Control/BezierPath.cs
+++ Assets/SidusTest/Code/Control/BezierPath.cs
@@ -6,7 +6,7 @@ namespace SidusTest.Control
     {
         [SerializeField] private Vector2 bezierRadius = new Vector2(1, 3);
 
-        private readonly Vector3[] _bezierPoints = new Vector3[4];
+        private readonly Vector3[] _bezierPoints = new Vector3[3];
 
         private static BezierPath _instance;
 
@@ -20,24 +20,24 @@ namespace SidusTest.Control
 
         private void PreparePathImpl(Vector3 from, Vector3 to)
         {
-            var continueCurve = Vector3.Distance(from, _bezierPoints[3]) < float.Epsilon;
+            var continueCurve = Vector3.Distance(from, _bezierPoints[2]) < float.Epsilon;
             _bezierPoints[0] = from;
-            _bezierPoints[3] = to;
-            var middlePoint = (from + to) / 2;
-            var middle1 = (from + middlePoint) / 2;
-            var middle2 = (to + middlePoint) / 2;
-            _bezierPoints[1] = continueCurve ? MirrorPoint(_bezierPoints[2], from) : ShiftPoint(middle1);
-            _bezierPoints[2] = ShiftPoint(middle2);
+            _bezierPoints[1] = continueCurve ? MirrorPoint(_bezierPoints[1], from) : ShiftPoint((from + to) / 2);
+            _bezierPoints[2] = to;
         }
 
         private Vector3 MirrorPoint(Vector3 previous, Vector3 current)
         {
-            var next = 2 * current - previous;
+            var direction = (current - previous).normalized;
+            var next = current + direction * RandomMultiplier();
             return next;
         }
 
         private Vector3 ShiftPoint(Vector3 middle1) 
-            => Random.Range(bezierRadius.x, bezierRadius.y) * Random.onUnitSphere + middle1;
+            => RandomMultiplier() * Random.onUnitSphere + middle1;
+
+        private float RandomMultiplier() 
+            => Random.Range(bezierRadius.x, bezierRadius.y);
 
         public static Vector3 PositionAt(float t)
             => _instance.PositionAtImpl(t);
@@ -46,14 +46,11 @@ namespace SidusTest.Control
         {
             var u = 1 - t;
             var uu = u * u;
-            var uuu = uu * u;
             var tt = t * t;
-            var ttt = tt * t;
 
-            var p = uuu * _bezierPoints[0]
-                + 3 * uu * t * _bezierPoints[1]
-                + 3 * u * tt * _bezierPoints[2]
-                + ttt * _bezierPoints[3];
+            var p = uu * _bezierPoints[0]
+                    + 2 * u * t * _bezierPoints[1]
+                    + tt * _bezierPoints[2];
 
             return p;
         }
